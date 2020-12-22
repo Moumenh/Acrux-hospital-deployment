6521a13d8338511a15ace61c3849d6e311abe79e
Moumenh <mohanbali94@gmail.com>
2020-12-22 19:48:19 +0200

    deploy

--- core/settings.py
+++ core/settings.py
@@ -209,9 +209,9 @@ CHANNEL_LAYERS = {
     }
 }
 
-# import dj_database_url 
-# prod_db  =  dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(prod_db)
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
 
 if 'DATABASE_URL' in os.environ:
     import dj_database_url
